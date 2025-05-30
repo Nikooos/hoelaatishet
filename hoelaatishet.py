57b6324d4af14f1c64a2ed9151a9e669215c0b23
Nico Martens <nico.martens@basic-fit.com>
2025-05-30 15:27:11 +0200

    remove unneeded prints

--- hoelaatishet.py
+++ hoelaatishet.py
@@ -9,8 +9,6 @@ load_dotenv()
 BLSKY_HANDLE = os.environ.get('BLSKY_HANDLE')
 BLSKY_APP_PASSWORD = os.environ.get('BLSKY_APP_PASSWORD')
 
-print ("Bluesky handle:", BLSKY_HANDLE)
-print ("Bluesky app password:", BLSKY_APP_PASSWORD)
 now = datetime.now()
 now = datetime.now(ZoneInfo("Europe/Amsterdam"))
 current_hour = now.hour
@@ -19,8 +17,8 @@ print("Het is nu " + str(current_hour) + ":00 uur")
 
 post_text = f"Het is nu {current_hour}:00 uur #hoelaatishet"
 
-# bsky_client = Client()
-# bsky_client.login(BLSKY_HANDLE, BLSKY_APP_PASSWORD)
+bsky_client = Client()
+bsky_client.login(BLSKY_HANDLE, BLSKY_APP_PASSWORD)
 
-# bsky_client.send_post(text=post_text)
-# print("Geplaatst op Bluesky ✅")
+bsky_client.send_post(text=post_text)
+print("Geplaatst op Bluesky " str(current_hour))
