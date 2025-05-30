1c0a266e97bc998896c126d2e1530c205ee173e0
Nico Martens <nico.martens@basic-fit.com>
2025-05-30 15:26:03 +0200

    add zoneinfo

--- hoelaatishet.py
+++ hoelaatishet.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from atproto import Client
 from datetime import datetime
+from zoneinfo import ZoneInfo
 
 load_dotenv()
 
@@ -11,14 +12,15 @@ BLSKY_APP_PASSWORD = os.environ.get('BLSKY_APP_PASSWORD')
 print ("Bluesky handle:", BLSKY_HANDLE)
 print ("Bluesky app password:", BLSKY_APP_PASSWORD)
 now = datetime.now()
+now = datetime.now(ZoneInfo("Europe/Amsterdam"))
 current_hour = now.hour
 
 print("Het is nu " + str(current_hour) + ":00 uur")
 
 post_text = f"Het is nu {current_hour}:00 uur #hoelaatishet"
 
-bsky_client = Client()
-bsky_client.login(BLSKY_HANDLE, BLSKY_APP_PASSWORD)
+# bsky_client = Client()
+# bsky_client.login(BLSKY_HANDLE, BLSKY_APP_PASSWORD)
 
-bsky_client.send_post(text=post_text)
-print("Geplaatst op Bluesky ✅")
+# bsky_client.send_post(text=post_text)
+# print("Geplaatst op Bluesky ✅")
