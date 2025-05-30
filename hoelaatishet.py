05d4d0bb7a28e863176278eeed450c208cd5164a
Nico Martens <nico.martens@basic-fit.com>
2025-05-30 15:22:05 +0200

    Backup before merge

---
+++ hoelaatishet.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+from atproto import Client
+from datetime import datetime
+
+load_dotenv()
+
+BLSKY_HANDLE = os.environ.get('BLSKY_HANDLE')
+BLSKY_APP_PASSWORD = os.environ.get('BLSKY_APP_PASSWORD')
+
+print ("Bluesky handle:", BLSKY_HANDLE)
+print ("Bluesky app password:", BLSKY_APP_PASSWORD)
+now = datetime.now()
+current_hour = now.hour
+
+print("Het is nu " + str(current_hour) + ":00 uur")
+
+post_text = f"Het is nu {current_hour}:00 uur #hoelaatishet"
+
+bsky_client = Client()
+bsky_client.login(BLSKY_HANDLE, BLSKY_APP_PASSWORD)
+
+bsky_client.send_post(text=post_text)
+print("Geplaatst op Bluesky ✅")
