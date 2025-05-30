30465f556550e74294b785d7f7dabbc20f8c86f1
Nico Martens <nico.martens@basic-fit.com>
2025-05-30 15:39:59 +0200

    fix script

--- hoelaatishet.py
+++ hoelaatishet.py
@@ -21,4 +21,4 @@ bsky_client = Client()
 bsky_client.login(BLSKY_HANDLE, BLSKY_APP_PASSWORD)
 
 bsky_client.send_post(text=post_text)
-print("Geplaatst op Bluesky " str(current_hour))
+print("Geplaatst op Bluesky ", str(current_hour))
