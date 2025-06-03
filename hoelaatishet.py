be6ecdbc51d6e22aa2bd24df9dc250c3a97be49f
Nico Martens <nico.martens@basic-fit.com>
2025-06-03 13:12:28 +0200

    UPDATE add twitter posting

--- hoelaatishet.py
+++ hoelaatishet.py
@@ -3,11 +3,18 @@ from dotenv import load_dotenv
 from atproto import Client
 from datetime import datetime
 from zoneinfo import ZoneInfo
+import tweepy
 
 load_dotenv()
 
 BLSKY_HANDLE = os.getenv('BLSKY_HANDLE')
 BLSKY_APP_PASSWORD = os.getenv('BLSKY_APP_PASSWORD')
+
+TWITTER_API_KEY = os.getenv("TWITTER_API_KEY")
+TWITTER_API_SECRET = os.getenv("TWITTER_API_SECRET")
+TWITTER_ACCESS_TOKEN = os.getenv("TWITTER_ACCESS_TOKEN")
+TWITTER_ACCESS_SECRET = os.getenv("TWITTER_ACCESS_SECRET")
+TWITTER_BEARER_TOKEN = os.getenv("TWITTER_BEARER_TOKEN")
 
 now = datetime.now()
 now = datetime.now(ZoneInfo("Europe/Amsterdam"))
@@ -47,4 +54,20 @@ bsky_client = Client()
 bsky_client.login(BLSKY_HANDLE, BLSKY_APP_PASSWORD)
 
 bsky_client.send_post(text=post_text, facets=facets)
-print("Geplaatst op Bluesky ", str(current_hour))
+print("✅ Geplaatst op Bluesky ", str(current_hour))
+
+# only post tweets on even hours
+if current_hour % 2 != 0:
+    print(f"⏸️ Sla over: {current_hour} is een oneven uur.")
+    exit(0)
+
+client = tweepy.Client(
+    bearer_token=TWITTER_BEARER_TOKEN,
+    consumer_key=TWITTER_API_KEY,
+    consumer_secret=TWITTER_API_SECRET,
+    access_token=TWITTER_ACCESS_TOKEN,
+    access_token_secret=TWITTER_ACCESS_SECRET
+)
+
+response = client.create_tweet(text=post_text)
+print("✅ Tweet geplaatst! ID:", response.data['id'])
